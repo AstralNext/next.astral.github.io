4c29b2f5ac48e8db1474edf721e40c48442b6e05
会做饭的二哈 <baikaiwen12@outlook.com>
2026-08-27 13:30:09 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://next.astral.fan',
   output: 'static',
   compressHTML: true,
+  integrations: [sitemap()],
 });
